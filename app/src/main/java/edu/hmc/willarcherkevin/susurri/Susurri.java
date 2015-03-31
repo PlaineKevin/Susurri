e4639f3cb70b629b420a5e0b72e7f953b583c30b
willmarks <williamgmarks@gmail.com>
2015-03-30 18:29:50 -0700

    Added ParseUsers to the application.java object

--- app/src/main/java/edu/hmc/willarcherkevin/susurri/Susurri.java
+++ app/src/main/java/edu/hmc/willarcherkevin/susurri/Susurri.java
@@ -2,15 +2,25 @@ package edu.hmc.willarcherkevin.susurri;
 
 import android.app.Application;
 import android.content.res.Configuration;
+import android.provider.Settings;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 /**
  * Created by archerwheeler on 3/29/15.
  */
 public class Susurri extends Application {
+
+    //Store the device unique user ID
+    public static String androidId;
+
+    //Create a global ParseUser object
+    public ParseUser theUser;
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -20,6 +30,8 @@ public class Susurri extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        setUpUsers();
         // Initialize the Parse SDK.
         Parse.initialize(this, "9nWnCUTdcZrrXtlGQKOjgPJWayPRKyMSQzU2bXhX", "dCjilcjkIqYAlyx55CIwFqyVjzl1GvKAuML64sXo");
         ParseACL defaultACL = new ParseACL();
@@ -41,6 +53,52 @@ public class Susurri extends Application {
     @Override
     public void onTerminate() {
         super.onTerminate();
+        logoutUsers();
+    }
+
+
+    // setUpUsers first checks to see if a ParseUser exists and if so it will log the user in
+    // otherwise, it creates a new ParseUser with the username and password being the androidID
+    private void setUpUsers() {
+        ParseUser.logInInBackground(androidId, androidId, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Hooray! The user is logged in.
+                } else {
+                    theUser = new ParseUser();
+                    user.setUsername(androidId);
+                    user.setPassword(androidId);
+
+                    // other fields can be set just like with ParseObject
+                    user.put("avatar", "Snail");
+
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                // Hooray! Let them use the app now.
+                            } else {
+                                // Sign up didn't succeed. Look at the ParseException
+                                // to figure out what went wrong
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void resumeUsers() {
+        theUser = ParseUser.getCurrentUser();
+        if (theUser != null) {
+            // Cool Beans the user is restored
+        } else {
+            setUpUsers();
+        }
+    }
+
+    private void logoutUsers() {
+        ParseUser.logOut();
+        theUser = ParseUser.getCurrentUser(); // this will now be null
     }
 
 }
